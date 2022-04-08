d8888f472c2abb8f0da44b1e07b95125bb77a5c8
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-04-08 12:35:14 -0400

    update socket.js

--- logic/socket.js
+++ logic/socket.js
@@ -1,8 +1,18 @@
 const config = require("../config.js");
 const io = require(config.DIRNAME + "/server.js");
+const auth = require(config.LOGIC + "/auth/authenticator.js");
+const DB = require(config.LOGIC + "/helpers/DB.js");
 
 io.on("connection" , (socket) => {
-    console.log(socket);
+    const token = socket.handshake.query.token;
+    if(!token) return socket.disconnect();
+    
+    const id = auth.verify(token);
+    if(!id) return socket.disconnect();
+    
+    if(!DB.findUserById(id)) return socket.disconnect();
+    
+    
 });
 
 module.exports = true;
