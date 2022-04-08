5ab435fff5c778e53a21276ed02a8e1a0e2a0427
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-04-08 17:20:33 -0400

    update load.js

--- logic/socket/load.js
+++ logic/socket/load.js
@@ -1,3 +1,10 @@
 /******************
  * On Socket Load *
  ******************/
+
+const config = require("../../config.js");
+const DB = require(config.LOGIC + "/helper/DB.js");
+
+const load = (io , socket , id) => {
+    
+};
