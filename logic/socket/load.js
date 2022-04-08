beaac21fe333c6cd4e34b04e0bda14f2768394bf
Franky96lol <90977263+Franky96lol@users.noreply.github.com>
2022-04-08 19:33:46 -0400

    update load.js

--- logic/socket/load.js
+++ logic/socket/load.js
@@ -6,5 +6,10 @@ const config = require("../../config.js");
 const DB = require(config.LOGIC + "/helper/DB.js");
 
 const load = (io , socket , id) => {
+    const firstEnter = DB.getUserValue(id ,"firstEnter");
+    if(firstEnter) {
+        socket.emit("firstEnter" , true);
+    }
+    
     
 };
